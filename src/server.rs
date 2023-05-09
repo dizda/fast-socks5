59c38275a73c707440100986d66559fb628c5048
nemosupremo <nimiwaribokoj@gmail.com>
2023-05-09 04:25:53 +0000

    Add into_inner on Socks5Socket

--- src/server.rs
+++ src/server.rs
@@ -249,6 +249,11 @@ impl<T: AsyncRead + AsyncWrite + Unpin> Socks5Socket<T> {
         Ok(self)
     }
 
+    /// Consumes the `Socks5Socket`, returning the wrapped stream.
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
+
     /// Read the authentication method provided by the client.
     /// A client send a list of methods that he supports, he could send
     ///
